e7093f1dbe9b3f4b1efc2dd6ad8740c8aa326459
sujay4d <sujay.chauhan@gmail.com>
2016-08-23 14:58:46 -0500

    testblue

--- image.php
+++ image.php
@@ -6,12 +6,11 @@ $blue = imagecolorallocate($im, 0, 0, 255);
 $green = imagecolorallocate($im, 0, 255, 0);
 
 // Draw a filled rectangle
-//imagefilledrectangle($im, 0, 0, 199, 199, $blue);
-imagefilledrectangle($im, 0, 0, 199, 199, $green);
+imagefilledrectangle($im, 0, 0, 199, 199, $blue);
+//imagefilledrectangle($im, 0, 0, 199, 199, $green);
 
 // Save the image
 header('Content-Type: image/png');
 imagePNG($im);
-SUJAY SUJAY
 imagedestroy($im);
 ?>
